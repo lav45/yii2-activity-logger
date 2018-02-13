74c1e57d009f937211f253f2d0aeedab04ca51e0
LAV45 <LAV451@gmail.com>
2018-02-13 23:01:50 +0300

    compression migrations

--- migrations/m170427_214256_create_activity_log.php
+++ migrations/m170427_214256_create_activity_log.php
@@ -18,17 +18,19 @@ class m170427_214256_create_activity_log extends Migration
     public function safeUp()
     {
         $this->createTable('{{%activity_log}}', [
+            'id' => $this->bigPrimaryKey(),
             'entity_name' => $this->string(32)->notNull(),
             'entity_id' => $this->string(32),
             'created_at' => $this->integer()->notNull(),
             'user_id' => $this->string(32),
             'user_name' => $this->string(255),
             'action' => $this->string(32),
+            'env' => $this->string(32),
             'data' => $this->text(),
         ], $this->tableOptions);
 
         $this->createIndex('activity_log-entity_name-entity_id-idx', '{{%activity_log}}', ['entity_name', 'entity_id']);
-        $this->createIndex('activity_log-created_at', '{{%activity_log}}', 'created_at');
+        $this->createIndex('activity_log-user_id', '{{%activity_log}}', 'user_id');
     }
 
     public function safeDown()
